025a1b60a99e3ad2a236cb123bb31dab0801535b
Roksolana <adelaw283@gmail.com>
2025-07-10 18:56:44 +0300

    app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,59 +1,62 @@
+import { useState } from "react";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import Loader from "../Loader/Loader";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import SearchBar from "../SearchBar/SearchBar";
-import type { Movie } from "../../types/movie";
+import MovieModal from "../MovieModal/MovieModal";
 import { fetchMovies } from "../../services/movieService";
-import { useState } from "react";
-import css from './App.module.css';
+import type { Movie } from "../../types/movie";
+import css from "./App.module.css";
 import { Toaster } from "react-hot-toast";
 import toast from "react-hot-toast";
-import MovieModal from "../MovieModal/MovieModal";
 
 export default function App() {
-    const [movies, setMovies] = useState<Movie[]>([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isError, setIsError] = useState(false);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
-    const handleSearch = async (query: string) => {
-        try {
-            setIsLoading(true);
-            setIsError(false);
-            setMovies([]);
+  const handleSearch = async (query: string) => {
+    try {
+      setIsLoading(true);
+      setIsError(false);
+      setMovies([]);
 
-            const data = await fetchMovies(query);
+      const data = await fetchMovies(query);
 
-            if (data.length === 0) {
-                toast.error('No movies found for your request.');
-            }
-            setMovies(data);
-        } catch {
-            setIsError(true);
-        } finally {
-            setIsLoading(false);
-        }
-    };
+      if (data.length === 0) {
+        toast.error("На жаль, фільми за вашим запитом не знайдено.");
+      }
 
-    const handleSelectedMovie = (movie: Movie) => {
-        setSelectedMovie(movie);
-        setIsModalOpen(true);
-    };
+      setMovies(data);
+    } catch {
+      setIsError(true);
+      toast.error("Сталася помилка. Спробуйте ще раз пізніше.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    const closeModal = () => {
-        setSelectedMovie(null);
-        setIsModalOpen(false);
-    };
+  const handleSelectedMovie = (movie: Movie) => {
+    setSelectedMovie(movie);
+  };
 
-    return (
-        <div className={css.app}>
-            <SearchBar onSubmit={handleSearch} />
-            <Toaster />
-            {isLoading && <Loader />}
-            {isError && <ErrorMessage />}
-            {movies.length > 0 && <MovieGrid onSelect={handleSelectedMovie} movies={movies} />}
-            {isModalOpen && selectedMovie && <MovieModal movie={selectedMovie} onClose={closeModal} />}
-        </div>
-    );
+  const closeModal = () => {
+    setSelectedMovie(null);
+  };
+
+  return (
+    <div className={css.app}>
+      <SearchBar onSubmit={handleSearch} />
+      <Toaster />
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      {movies.length > 0 && (
+        <MovieGrid onSelect={handleSelectedMovie} movies={movies} />
+      )}
+      {selectedMovie && (
+        <MovieModal movie={selectedMovie} onClose={closeModal} />
+      )}
+    </div>
+  );
 }
